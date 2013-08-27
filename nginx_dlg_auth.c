e6f041ef89e5924f70ac00a307b5859e28bca817
Jan Algermissen <algermissen@acm.org>
2013-08-27 16:07:08 +0200

    Removed unused variable

--- nginx_dlg_auth.c
+++ nginx_dlg_auth.c
@@ -213,7 +213,6 @@ static char * ngx_http_dlg_auth_iron_passwd(ngx_conf_t *cf, ngx_command_t *cmd, 
 static ngx_int_t ngx_http_dlg_auth_init(ngx_conf_t *cf) {
     ngx_http_handler_pt        *h;
     ngx_http_core_main_conf_t  *cmcf;
-        ngx_str_t vn;
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
     if( (h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers)) == NULL) {
