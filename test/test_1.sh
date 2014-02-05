56dba98cd68b67e1e88e345e7438f40b859488ac
Jan Algermissen <algermissen@acm.org>
2014-02-05 22:27:59 +0100

    Renamed test files

--- test/test_1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-if [ `curl -s http://localhost/unprotected -w "%{http_code}" -o /dev/null` -ne 200 ] ; then echo "Expected 200"; exit 1;  fi
-
--- test/test_2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-if [ `curl -s -H "$AUTH" http://localhost/protected -w "%{http_code}" -o /dev/null` -ne 200 ] ; then echo "Expected 200"; exit 1;  fi
-
